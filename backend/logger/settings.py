0dd43a867af56f366045bcee22596eac4f8f6328
Hrishikesh Dasharath Pawar <88403078+hrishikeshp20@users.noreply.github.com>
2025-04-17 21:46:58 +0530

    Update settings.py

--- backend/logger/settings.py
+++ backend/logger/settings.py
@@ -24,6 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://discord-bot-logger-backend.onrender.com",
+]
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
