6e7793ef9665a21413c9fb6998a646164150f80b
Hrishikesh Dasharath Pawar <88403078+hrishikeshp20@users.noreply.github.com>
2025-04-18 09:47:38 +0530

    Update models.py

--- backend/monitor/models.py
+++ backend/monitor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+import pytz
 
 class PingLog(models.Model):
     SOURCE_CHOICES = [
@@ -8,8 +9,14 @@ class PingLog(models.Model):
     ]
 
     source = models.CharField(max_length=20, choices=SOURCE_CHOICES)
-    timestamp = models.DateTimeField(default=timezone.now)
+    timestamp = models.DateTimeField()
     message = models.TextField(blank=True, null=True)
 
+    def save(self, *args, **kwargs):
+        # Set timestamp to IST manually
+        ist = pytz.timezone('Asia/Kolkata')
+        self.timestamp = timezone.now().astimezone(ist)
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return f"{self.source} at {self.timestamp}"
+        return f"{self.source} at {self.timestamp.strftime('%Y-%m-%d %H:%M:%S')}"
