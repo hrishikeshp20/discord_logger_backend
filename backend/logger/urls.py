bbd7e6dbe256d3874ee5e7161381de75fd858c73
Hrishikesh Dasharath Pawar <88403078+hrishikeshp20@users.noreply.github.com>
2025-04-29 16:02:00 +0530

    Update urls.py

--- backend/logger/urls.py
+++ backend/logger/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include  # 👈 include is the key
-
+from django.urls import path, include  
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('database.urls')),  # 👈 includes all routes from logger/urls.py
+    path('api/', include('database.urls')),  
     path('', include('monitor.urls')),
 
 ]
