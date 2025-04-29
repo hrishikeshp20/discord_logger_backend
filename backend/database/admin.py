d99f3fdeb414dd0ca63511a8f299a5a229e3e5cd
Hrishikesh Dasharath Pawar <88403078+hrishikeshp20@users.noreply.github.com>
2025-04-29 15:57:35 +0530

    Update admin.py

--- backend/database/admin.py
+++ backend/database/admin.py
@@ -3,6 +3,6 @@ from .models import DiscordUser
 
 @admin.register(DiscordUser)
 class DiscordUserAdmin(admin.ModelAdmin):
-    list_display = ("user_id", "user_name", "join_count", "status")  # 👈 Columns you want to see
-    search_fields = ("user_name", "user_id")                         # Optional: search box
-    list_filter = ("status",)                                        # Optional: filter sidebar
+    list_display = ("user_id", "user_name", "join_count", "status") 
+    search_fields = ("user_name", "user_id")                         
+    list_filter = ("status",)                                      
